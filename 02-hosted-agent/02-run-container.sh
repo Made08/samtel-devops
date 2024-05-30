2bfb844ebfdbb2a5921a63a53dbae1776ed8256d
Madelem Chico <madelenchicovelasco@gmail.com>
2024-05-29 23:34:02 -0500

    08-Entregables OK

--- 02-hosted-agent/02-run-container.sh
+++ 02-hosted-agent/02-run-container.sh
@@ -1,6 +1,6 @@
 docker run -e AZP_URL=https://dev.azure.com/madelemchico/samtel-devops \
            -e AZP_TOKEN=<your-personal-access-token> \
-           -e AZP_POOL=<your-agent-pool> \
+           -e AZP_POOL=Self-hosted-MADELEM \
            -e AZP_AGENT_NAME=azure-devops-self-hosted-agent \
            -v /var/run/docker.sock:/var/run/docker.sock \
            -it azure-devops-self-hosted-agent:latest
